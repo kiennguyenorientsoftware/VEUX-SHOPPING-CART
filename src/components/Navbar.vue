f2ff089c0952ec6693f5bc43ca7271d07a510da4
Kien Nguyen Xuan <kien.nguyen@orientsoftware.com>
2019-05-21 11:37:11 +0700

    done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>    
+    <nav class="navbar navbar-expand-sm navbar-warning fixed-top bg-warning">
+
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li>
+             <a class="navbar-brand" href="#"><b>My Shop</b></a>
+          </li>
+            
+            
+          
+        </ul>
+      </div>
+    </nav>
+</template>    
---
+++ src/products.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name: "Apple",
+        id: 1,
+        price: 4,
+        quantity: 1
+    },
+    {
+        name: "Orange",
+        id: 2,
+        price: 1,
+        quantity: 4
+    },
+    {
+        name: "Grapes",
+        id: 3,
+        price: 7,
+        quantity: 2
+    },
+    {
+        name: "Banana",
+        id: 4,
+        price: 70,
+        quantity: 5
+    },
+    {
+        name: "Pineapple",
+        id: 5,
+        price: 20,
+        quantity: 3
+    },
+    {
+        name: "Guava",
+        id: 6,
+        price: 50,
+        quantity: 10
+    }
+]
